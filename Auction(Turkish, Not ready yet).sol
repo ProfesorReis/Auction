ff768c0094ffdf81b5f8b6355e355f04802ec33b
ProfesorReis <64315287+ProfesorReis@users.noreply.github.com>
2021-12-24 19:15:48 +0300

    Update Auction(Turkish, Not ready yet).sol

--- Auction(Turkish, Not ready yet).sol
+++ Auction(Turkish, Not ready yet).sol
@@ -1,55 +1,119 @@
 // SPDX-License-Identifier: Unlicansed
 pragma solidity ^0.8.7;
 
-contract MyTheme {
+contract Auction {
 
-    // Kontratın sahibi, "payable" ve "public" in yazılış sırası önemli neden bilmiyorum şimdilik.
-    address payable public owner;   // Bunu tanımlarken "payable" ekleyince constructor'da sorun çıkıyor. "payable" yi constructor'a ekleyince sorun olmuyor.
+    address payable public owner;
 
-    // Kontrata 3 adet durum belirledik, enum kullanıma hakim değilim kullanmak gerekmiyor olabilir.
-    enum Situation {Active, Deactive, Unkown}
-    Situation public contractSituation;
+    // Auction'un başlama ve bitiş zamanlarını hesaplamamızda kullanılacak değişkenler.
+    uint public startBlock;
+    uint public endBlock;
+
+    // Auction'un bilgileri, resimler vb. şeyleri blockchain'e kaydetmek pahalı olduğu için bu bilgileri kaydetmek için off chain bir çözüm kullanıyoruz.
+    string public ipfsHash;
+
+    enum State {Started, Running, Ended, Canceled}  // Olabilecek durumları tanımladık.
+    State public auctionState;  // Şu anki durumun nasıl olduğunu belirtebilmek için sonra kullanmak üzere bir değişken tanımladık fakat ona variable eklemedik daha.
+
+    uint public highestBindingBid;
+    address payable public highestBidder;
+
+    mapping(address => uint) public bids;
+    uint bindIncrement;
 
     constructor() {
         owner = payable(msg.sender);
-        contractSituation = Situation.Active;
+        auctionState = State.Started;
+        startBlock = block.number; // "block.number" şu anki bloğun numarasını alıyor bu da ilk blok olmuş oluyor.
+        endBlock = startBlock + 40320; // Her bir blok için ortalama 15 saniye geçiyormuş böylelikle bitiş tarihi hesaplandı
+        ipfsHash = "";  // "I'm initializing it to an empty string" ???
+        bindIncrement = 100; // Bid arttırmak için 100 wei ödenmesi gerekiyor.
     }
-
-    // Olmazsa olmazlar, kontratın transfer kabul edebilmesi için gereken fonksiyonlar
-    receive() external payable{
-    }
-    fallback() external payable{
-    }
-
-    // Sadece "owner" address'in yapabileceği şeyleri belirlemek için modifer tanımladık.
-    modifier onlyOwner {
-        require(owner == msg.sender);   // "==" iki eşittire dikkat et, tek eşittir yaptım diye 2 saat sıkıntı ne diye aradım.
-        _;
-    }
-
-    // "owner" ın çağıramayacağı fonksiyonları belirlemek için modifier tanımladık.
+    
+    // "owner" ın yapamayacağı şeyleri belirlemek için tanımladık.
     modifier notOwner() {
-        require(owner != msg.sender);
+        require(msg.sender != owner);
         _;
     }
 
-    // Kontratın bakiyesini gösteren fonksiyon.
-    function getBalance() public view returns(uint) {
-        return address(this).balance;
+    // Fonksiyonun çalışabilmesi için "startBlock" ve "endBlock" arasında olması gerektiğini tanımladık
+    modifier afterStart() {
+        require(block.number >= startBlock);
+        _;
+    }
+    modifier beforeEnd() {
+        require(block.number <= endBlock);
+        _;
     }
 
-    // Buradan emin değilim?
-    // Kontrattan birine Eth yollamak için gereken fonksiyon.
-    // Sadece owner kullanabilmeli bu fonksiyonu.
-    function sendEther(address payable _recipient, uint _amount) public onlyOwner returns(bool) {
-        require(owner == msg.sender, "Transfer failed, you are not the owner!");
+    // Sadece owner'ın yapabileceği şeyler için modifier tanımladık.
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 
-        // Eğer yollanacak miktar balance'den küçükse işlem gerçekleştirilir değilse iptal edilir.
-        if (_amount <= getBalance()) {
-            _recipient.transfer(_amount);
-            return true;
+    // Solidity'de iki sayıdan küçük olanı bulmak için bir fonskiyon yok o yüzden biz kendimize tanımlıyoruz.
+    function min(uint a, uint b) pure internal returns(uint) {
+        if(a <= b) {
+            return a;
         } else {
-            return false;
+            return b;
         }
+    }
+
+    // Sadece owner'ın kullanabileceği bir iptal etme fonksiyonu tanımladık.
+    function cancelAuction() public onlyOwner {
+        auctionState = State.Canceled;
+    }
+
+    // İlk komplike kodlarımdan biri olduğu için kafam biraz karışık ve bazı yerleri yanlış açıklamış olabilirim.
+    // Bu fonksiyon bizim ana fonksiyonumuz. Bid yapma işlemlerini halledicek.
+    function placeBid() public payable notOwner afterStart beforeEnd {
+        require(auctionState == State.Running); // Başlayabilmesi için önceden tanımladığımız enum'u çağırdık.
+        require(msg.value >= 100);  // Minimum bid'i 100 wei olarak ayarladık
+
+        uint currentBid = bids[msg.sender] + msg.value; // Kullanıcının şu anki bid'ini görebilmesi için önceden tanımladığımız mapping'i çağırıp, önceki bid'ine şu an yaptığı bid'i ekleyen bir local variable tanımladık.
+        require(currentBid > highestBindingBid); // En son bid'i en yüksek bid'den yüksek olması gerektiğini tanımladık.
+
+        bids[msg.sender] = currentBid;  // mapping'i çağırarak şu anki bid'le fonksiyonu kullanan kişiyi birlieştirdik?
+
+        // currentBid en yüksek bid'den yüksek olması gerekiyor.
+        if(currentBid <= bids[highestBidder]) {
+            highestBindingBid = min(currentBid + bindIncrement, bids[highestBidder]);
+        } else {
+            highestBindingBid = min(currentBid, bids[highestBidder] + bindIncrement);
+            highestBidder = payable(msg.sender);
+        }
+
+
+    }
+
+    function finalizeAuction() public {
+        require(auctionState == State.Canceled || block.number > endBlock); // "||" = veya anlamına geliyor. Burada ya auction Canceled olmalıdır ya da block.number endBlock'dan büyük olmalıdır (belirlenen süre dolmalıdır) ki işlem gerçekleşsin.
+        require(msg.sender == owner || bids[msg.sender] > 0);   // Ya owner bu fonksyionu çağırabilir ya da bid yapmış herhangi birisi.
+    
+        address payable recipient;  // Fonksiyonu kullanan adresi belirlemek için bunu tanımladık.
+        uint value; //  Fonkisyonu kullananın value'sini belirlemek için bunu tanımladık.
+
+        // Fonkiyonu çağıracak kişiler için ayrı ayrı senaryo oluşturduk.
+        if(auctionState == State.Canceled) {
+            recipient = payable(msg.sender);
+            value = bids[msg.sender];
+        } else {
+            if(msg.sender == owner) {
+                recipient = owner;
+                value = highestBindingBid;  // Belki "value = bids[highestBidder] - highestBindingBid;" olara değiştirilmeli?
+            } else {
+                if(msg.sender == highestBidder) {
+                    recipient = highestBidder;
+                    value = bids[highestBidder] - highestBindingBid;
+                } else {
+                    recipient = payable(msg.sender);
+                    value = bids[msg.sender];
+                }
+            }
+        }
+
+        recipient.transfer(value);  // Fonksiyonu kullanan kişiye parasını yolladık?
     }
 }
